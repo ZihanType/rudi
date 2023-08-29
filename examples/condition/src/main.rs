c253284f3350aed3f6024407c2a0eb70b12761d0
ZihanType <ZihanType@protonmail.com>
2023-08-29 13:50:40 +0800

    example: add condition example

---
+++ examples/condition/src/main.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashMap, env, fmt::Debug, rc::Rc};
+
+use rudi::{Context, Singleton, Transient};
+
+#[derive(Clone)]
+struct Environment {
+    map: HashMap<String, String>,
+}
+
+#[Singleton(eager_create)]
+impl Environment {
+    fn new() -> Self {
+        Self {
+            map: env::vars().collect(),
+        }
+    }
+}
+
+trait Service: Debug {}
+
+fn transfer<T: Service + 'static>(t: T) -> Rc<dyn Service> {
+    Rc::new(t)
+}
+
+fn condition(cx: &Context, value: &str) -> bool {
+    cx.get_singleton::<Environment>()
+        .map
+        .get("env")
+        .map(|a| a == value)
+        .unwrap_or(false)
+}
+
+#[derive(Debug)]
+#[Transient(condition = |cx| condition(cx, "dev"), binds = [transfer])]
+struct A;
+
+impl Service for A {}
+
+#[derive(Debug)]
+#[Transient(condition = |cx| condition(cx, "prod"), binds = [transfer])]
+struct B;
+
+impl Service for B {}
+
+fn main() {
+    env::set_var("env", "dev");
+    let mut cx = Context::auto_register();
+    let svc = cx.resolve::<Rc<dyn Service>>();
+    assert_eq!(format!("{:?}", svc), "A");
+
+    env::set_var("env", "prod");
+    let mut cx = Context::auto_register();
+    let svc = cx.resolve::<Rc<dyn Service>>();
+    assert_eq!(format!("{:?}", svc), "B");
+}
